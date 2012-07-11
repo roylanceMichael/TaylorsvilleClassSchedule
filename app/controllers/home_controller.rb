c07245ac0ac1c13db831650f04232081b44551e5
Mike Roylance <roylance.michael@gmail.com>
2012-07-11 10:18:39 -0600

    latest changes!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,22 @@
 
 class HomeController < ApplicationController
   def index
+   @currentSemester = Semester.last
+   @timeSlots = TimeSlot.all
+   @semesterClassViewModel = []
+   Department.all.each do |dept|
+      semClasses = []
+        dept.SchoolClasses.each do |schoolClass|
+          SemesterClass.all.find_all { |sc| sc.SchoolClassId == schoolClass.id && sc.SemesterId == @currentSemester.id }.each do |semesterClass|
+            semClasses.push(semesterClass)
+          end
+        end
+        anonType = {
+          Department: dept,
+          SemesterClasses: semClasses
+        }
+
+        @semesterClassViewModel.push(anonType)
+   end
   end
 end
--- db/migrate/20120708220659_create_semester_classes.rb
+++ db/migrate/20120708220659_create_semester_classes.rb
@@ -1,8 +1,6 @@
 class CreateSemesterClasses < ActiveRecord::Migration
   def change
     create_table :semester_classes do |t|
-      t.string :Name
-      t.string :Description
       t.integer :SemesterId
       t.integer :TimeSlotId
       t.integer :SchoolClassId
